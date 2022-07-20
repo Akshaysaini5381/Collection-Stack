c42aa75ca9ce1509992157f66bf625c28797a415
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-07-20 23:24:00 +0530

    Add project files.

---
+++ Collection Stack/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Collection_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> s = new Stack<string>();
+            s.Push("akshay");
+            s.Push("d");
+            s.Push("z");
+            s.Push("akshay");
+           s.Push(null);
+            Console.WriteLine("  "+s.Count);
+            
+
+            
+
+               foreach (var item in s)
+            {
+                Console.Write(" "+item);
+            }
+
+
+            Console.WriteLine();
+            
+
+        }
+    }
+}
